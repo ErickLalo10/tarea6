7c4655fdc7980c0e92b091eec636265be044aa1f
ErickLalo10 <54512422+ErickLalo10@users.noreply.github.com>
2019-08-26 08:21:58 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,12 +1,1 @@
-<html>
-<head><title>Mi primer codigo JavaScript</title></head>
-<body>
- 
-<h1>Mi primer código JavaScript</h1>
- 
-<script>
-  document.write("Hola Mundo");
-</script>
- 
-</body>
-</html>
+console.log('hola mundo')
